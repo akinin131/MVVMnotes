aa18769b383e45dd7c7b2368493152ef391c5902
akininilya <akinin131@mail.ru>
2022-10-08 13:26:55 +0300

    three commitео

--- app/src/main/java/quiz/example/notesapp/screens/Start.kt
+++ app/src/main/java/quiz/example/notesapp/screens/Start.kt
@@ -1,5 +1,6 @@
 package quiz.example.notesapp.screens
 
+import android.app.Application
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.Scaffold
@@ -7,28 +8,42 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import org.intellij.lang.annotations.JdkConstants
 import quiz.example.notesapp.navigation.NavRout
 import quiz.example.notesapp.ui.theme.NotesAppTheme
+import quiz.example.notesapp.utils.TYPE_FIREBASE
+import quiz.example.notesapp.utils.TYPE_ROOM
+import quiz.example.notesapp.viewModel.MainViewModel
+import quiz.example.notesapp.viewModel.ResultViewModelFactory
 
 @Composable
 fun Start(navController: NavHostController) {
+
+    val context = LocalContext.current
+    val mViewModel: MainViewModel = viewModel(factory = ResultViewModelFactory(context.applicationContext as Application) )
+
     Scaffold(modifier = Modifier.fillMaxSize()) {
         Column(modifier = Modifier.fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center) {
             Text(text = "Что мы будем изпользовать?")
-            Button(onClick = { navController.navigate(route = NavRout.Main.route) },
+            Button(onClick = {
+                mViewModel.initDatabase(TYPE_ROOM)
+                navController.navigate(route = NavRout.Main.route) },
                 modifier = Modifier
                     .width(200.dp)
                     .padding(vertical = 22.dp)) {
                 Text(text = "Room DataBase")
             }
-            Button(onClick = { navController.navigate(route = NavRout.Main.route) },
+            Button(onClick = {
+                mViewModel.initDatabase(TYPE_FIREBASE)
+                navController.navigate(route = NavRout.Main.route) },
                 modifier = Modifier
                     .width(200.dp)
                     .padding(vertical = 2.dp)) {
